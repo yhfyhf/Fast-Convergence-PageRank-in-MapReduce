c6f04ce7e2f586b8f3438d8f48dbbd0a7e28dbdc
Haofei Ying <xdhfying@gmail.com>
2016-04-23 23:55:38 -0400

    refractory xyd's SimplePR Mapper

--- src/SimplePageRank/PageRankMapper.java
+++ src/SimplePageRank/PageRankMapper.java
@@ -24,26 +24,27 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text> {
     public void map(LongWritable keyIn, Text valueIn, Mapper.Context context)
             throws IOException, InterruptedException {
         System.out.println("!!valueIn: " + valueIn.toString());
-        String[] temp = valueIn.toString().trim().split(";");
+        String[] tokens = valueIn.toString().trim().split(";");
 
-        if (temp == null || temp.length < 2) { return; }
+        if (tokens.length < 2) {
+            return;
+        }
 
-        String srcNodeId = temp[0].trim();
-        String[] desNodeIds = temp[1].trim().split(",");
-        float srcNodePageRank = Float.parseFloat(temp[2].trim());
+        String srcNodeId = tokens[0].trim();
+        String[] desNodeIds = tokens[1].trim().split(",");
         int srcNodeDegree = desNodeIds.length;
+        float srcNodePageRank = Float.parseFloat(tokens[2].trim());
         float nextPageRank = srcNodePageRank / srcNodeDegree;
 
-
-        //emit the srcNodeInfo
+        // emit the srcNodeInfo
         Text keyOut = new Text(srcNodeId);
-        Text valueOut = new Text(Conf.NODEINFO + ";" + temp[1] + ";" + temp[2] + ";");
+        Text valueOut = new Text(Conf.NODEINFO + ";" + tokens[1] + ";" + tokens[2] + ";");
         context.write(keyOut, valueOut);
         System.out.println("[ Mapper ] key: " + keyOut + ", value: " + valueOut);
 
-        //emit the nextPageRank
-        //check if it has desNode
-        //if not, emit srcNode and its pageRank
+        // emit the nextPageRank
+        // check if it has desNode
+        // if not, emit srcNode and its pageRank
         if (desNodeIds.length == 0 || desNodeIds[0].isEmpty()) {
             valueOut = new Text(Conf.NEXTPAGERANK + ";" + nextPageRank);
             context.write(keyOut, valueOut);
